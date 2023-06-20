90e2639c5e2ef36680301562e52ad9eadcedbc2c
Eva Jaqueline Herrera de Martino <135066870+HerreraDeMartino@users.noreply.github.com>
2023-06-20 18:42:51 +0000

    Mapa

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+import csv
+import json
 
 def test_view(request):
     return render(request, "test.html", {})
 
-def map_view(request):
-    return render(request, "map.html", {})
+def map_view(request): 
+    data=[]
+    with open('SkinCancerbyCountries.csv', newline='') as csvfile:
+        spamreader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            print(row)
+            data.append({
+                "name": row["Country"],
+                "value": row["Melanoma Annual Incidence"]
+            })
+            
+        return render(request, "map.html", {"data": json.dumps(data)})
